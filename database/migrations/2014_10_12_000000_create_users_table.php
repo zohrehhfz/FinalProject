cd3db5138ca644052d35cd4a0ee5e5ba1d365e01
zohrehhfz <hfz.zohreh@gmail.com>
2023-02-01 20:29:42 +0330

    added photo,bio,phone, address columns to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -27,7 +27,8 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamps(); 
+            //added Extra columns to users table
         });
     }
 
