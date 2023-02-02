2c9762190901e186f4b257f7d4c7acb216057dd9
zohrehhfz <hfz.zohreh@gmail.com>
2023-02-02 20:10:01 +0330

    added name to towns table

--- database/migrations/2023_02_02_160638_create_towns_table.php
+++ database/migrations/2023_02_02_160638_create_towns_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('towns', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->timestamps();
         });
     }
