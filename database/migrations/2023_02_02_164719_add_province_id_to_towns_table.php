a84ce69e00c09b716a7f42e3e995f0d86c933558
zohrehhfz <hfz.zohreh@gmail.com>
2023-02-04 21:43:55 +0330

    added province id foreign key to towns table

---
+++ database/migrations/2023_02_02_164719_add_province_id_to_towns_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('towns', function (Blueprint $table) {
+            
+            $table->unsignedBigInteger('province_id')->after('id');
+			$table->foreign('province_id')->references('id')->on('provinces')->cascadeOnDelete();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('towns', function (Blueprint $table) {
+            //
+        });
+    }
+};
