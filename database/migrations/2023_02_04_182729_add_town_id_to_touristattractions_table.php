0d9c8bf0f13c4f1a771636b6451263bee9fecf81
zohrehhfz <hfz.zohreh@gmail.com>
2023-02-05 21:19:03 +0330

    added town_id to touristattractions table

---
+++ database/migrations/2023_02_04_182729_add_town_id_to_touristattractions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('touristattractions', function (Blueprint $table) {
+
+            $table->unsignedBigInteger('town_id')->after('name');
+			$table->foreign('town_id')->references('id')->on('towns')->cascadeOnDelete();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('touristattractions', function (Blueprint $table) {
+            //
+        });
+    }
+};
