a409b3ae5f9f90727e93db42cc6a081d69d1a3e4
zohrehhfz <hfz.zohreh@gmail.com>
2023-02-26 02:46:30 +0330

    added edit , update province method

--- app/Http/Controllers/ProvinceController.php
+++ app/Http/Controllers/ProvinceController.php
@@ -94,4 +94,80 @@ class ProvinceController extends Controller
 			return view('provinces.show', ['province' => $province , 'photo_url' => $url , 'role' => 0]);
 		}
 	}
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  \App\Models\Province  $province
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit(Province $province)
+	{
+		$user = Auth::user();
+
+		$rl = $user->role;
+		
+		if (($rl == "admin") || ($rl == "guide")) {
+			return view('provinces.edit', ['province' => $province]);
+		} else
+			return redirect()->back()->withErrors(['error' => 'شما اجازه تغییر اطلاعات این استان را ندارید']);
+	}
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \App\Models\Province  $province
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request,Province  $province)
+	{
+		$request->validate([
+			'name' => 'required|string|max:255',
+			'description' => 'required|string',
+			'photo' => ['required','mimes:jpg,png,jpeg,gif,svg', 'max:2048']
+		]);
+		
+		$newphotofilename = "null";
+		$photofilename = "null";
+		if ($request->photo != null) {
+							
+		$photofile = $request->file('photo');
+		$photofilename = $photofile->getClientOriginalName();
+		$extension = $photofile->extension();
+		$newphotofilename = sha1(time() . '_' . rand(1000000000, 1999999999) . '_' . rand(1000000000, 1999999999) . '_' . $photofilename);
+		$newphotofilename = $newphotofilename . '.' . $extension;
+
+		Storage::disk('local')->putFileAs(
+			'public/provinces',
+			$photofile,
+			$newphotofilename
+		);
+	}
+
+	DB::table('provinces')->where('id', $province->id)->update(array(
+
+		"name" => $request->name,
+		"description" => $request->description,
+		"photoname" => $newphotofilename,
+		"orginalphotoname" => $photofilename
+	));
+	
+	$url = Storage::url('public/provinces/' . $province->photoname);
+
+	if (Auth::user()) {
+		$user = Auth::user();
+		$rl = $user->role;
+
+		if (($rl == "admin") || ($rl == "guide")) {
+
+			return view('provinces.show', ['province' => $province , 'photo_url' => $url , 'role' => 1]);
+		}
+		else {
+			return view('provinces.show', ['province' => $province , 'photo_url' => $url , 'role' => 0]);
+		}
+	} 
+	else {
+		return view('provinces.show', ['province' => $province , 'photo_url' => $url , 'role' => 0]);
+	}
+	}
+
 }
