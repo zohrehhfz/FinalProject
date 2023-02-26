83dbfc565e18a38e6a02e8fe0cc1f458b4b75677
zohrehhfz <hfz.zohreh@gmail.com>
2023-02-26 23:08:16 +0330

    added list of provinces for create town

--- resources/views/towns/edit.blade.php
+++ resources/views/towns/edit.blade.php
@@ -60,6 +60,19 @@
             <textarea  name="description" rows="10" style="font-size:18px;" class="w-75">{{$town->description}} </textarea>
 			<br>
 			<br>
+
+			<div >
+			<p style="font-size:20px;"> نام استان  : </p>
+			<input list="provincess" name="province" id="provinces">
+			<datalist id="provincess">
+			@foreach($provinces as $province)
+    		<option value="{{$province->name}}"></option>
+			@endforeach
+  			</datalist>
+			<br>
+			<br>
+			</div>
+
 			<button type="submit" id="submitbutton" style="color:white;"> ثبت تغییرات <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-double-left" viewBox="0 0 16 16">
 					<path fill-rule="evenodd" d="M8.354 1.646a.5.5 0 0 1 0 .708L2.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z" />
 					<path fill-rule="evenodd" d="M12.354 1.646a.5.5 0 0 1 0 .708L6.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z" />
