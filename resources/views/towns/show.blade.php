029aabf21e2641054b96bb541f131262e47dc25a
zohrehhfz <hfz.zohreh@gmail.com>
2023-02-26 11:36:57 +0330

    create show town blade

---
+++ resources/views/towns/show.blade.php
@@ -0,0 +1,90 @@
+@extends('layouts.my_layout')
+@section('title', 'شهر')
+@section('content')
+<nav class="navbar navbar-expand-sm navbar-dark sticky-top">
+	<div class="container-fluid">
+		<ul class="navbar-nav">
+			<li class="nav-item">
+				@if (Route::has('login'))
+				@auth
+				@if(Auth::user()->role == "admin")
+				<a href="{{ route('CreateProvince') }}" class="navbar-brand"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
+						<path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z" />
+					</svg> افزودن استان </a>
+                <a href="{{ route('CreateTown') }}" class="navbar-brand"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
+						<path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z" />
+				</svg> افزودن شهر </a>
+				@endif
+				<a href="{{ url('/dashboard') }}" class="navbar-brand"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-lines-fill" viewBox="0 0 16 16">
+						<path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z" />
+					</svg> پنل کاربری </a>
+				@else
+				<a href="{{ route('login') }}" class="navbar-brand"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-key-fill" viewBox="0 0 16 16">
+						<path d="M3.5 11.5a3.5 3.5 0 1 1 3.163-5H14L15.5 8 14 9.5l-1-1-1 1-1-1-1 1-1-1-1 1H6.663a3.5 3.5 0 0 1-3.163 2zM2.5 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+					</svg> ورود به حساب </a>
+
+				@if (Route::has('register'))
+				<a href="{{ route('register') }}" class="navbar-brand"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-person-fill" viewBox="0 0 16 16">
+						<path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0zm2 5.755V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1v-.245S4 12 8 12s5 1.755 5 1.755z" />
+					</svg>
+					ثبت نام</a>
+				@endif
+				@endauth
+
+	</div>
+	@endif
+	</li>
+
+	</ul>
+	</div>
+</nav>
+
+<div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+	<div class="div1" style="width:70%; margin:auto;">
+		<br>
+		@if($photo_url == "/storage/provinces/")
+		<figure>
+			<img src="/travel-agency.jpg" class="img-fluid" style="margin:auto; width:25%; height:25%;" alt="profile photo Not Set">
+			<figcaption style="font-size: 14px;">تصویر یافت نشد</figcaption>
+		</figure>
+
+		@else
+		<img src={{$photo_url}} class="img-fluid img-circle2" style="margin:auto; width:60%; height:60%;" alt="Profile photo UnAvialable">
+		@endif
+		<br>
+		<br>
+		<p> نام شهر : {{$town->name}}</p>
+		<p> {{$town->description}}</p>
+		<br>
+		<br>
+		
+		@if($role == 1)
+				<button id="submitbutton"><a href="{{ route('EditTown',[$town]) }}" style="color:white; text-decoration: none;"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+							<path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
+						</svg> تغییر اطلاعات شهر </a></button>
+				@endif
+	</div>
+	<br>
+	<br>
+	<div class="comments container" style="margin: auto;">
+
+		<br>
+		<div class="sub_cm" style="background: none;">
+			<div class="row">
+				<div style="text-align: right;">
+					<h6> <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-collection-fill" viewBox="0 0 16 16">
+							<path d="M0 13a1.5 1.5 0 0 0 1.5 1.5h13A1.5 1.5 0 0 0 16 13V6a1.5 1.5 0 0 0-1.5-1.5h-13A1.5 1.5 0 0 0 0 6v7zM2 3a.5.5 0 0 0 .5.5h11a.5.5 0 0 0 0-1h-11A.5.5 0 0 0 2 3zm2-2a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7A.5.5 0 0 0 4 1z" />
+						</svg> &nbsp; نظرات</h6>
+				</div>
+			</div>
+		</div>
+		
+	<br>
+	<br>
+
+	<br>
+	<br>
+
+
+</div>
+@endsection
