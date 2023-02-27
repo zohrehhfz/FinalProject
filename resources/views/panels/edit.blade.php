feb8d11e7bc12f3fb0352212e6cd516e7d5b4d48
zohrehhfz <hfz.zohreh@gmail.com>
2023-02-27 21:52:27 +0330

    create edit page for edit profile

---
+++ resources/views/panels/edit.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.my_layout')
+@section('title', 'به روزرسانی حساب ')
+@section('content')
+
+<form method="POST" action="{{route('UpdateUserGuideInfo')}}" dir="rtl" enctype="multipart/form-data">
+    @csrf
+    <div class="container">
+        <div class="row divupload">
+            <div class=" col-6">
+                <p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-in-down" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M3.5 6a.5.5 0 0 0-.5.5v8a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-8a.5.5 0 0 0-.5-.5h-2a.5.5 0 0 1 0-1h2A1.5 1.5 0 0 1 14 6.5v8a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-8A1.5 1.5 0 0 1 3.5 5h2a.5.5 0 0 1 0 1h-2z"/>
+                 <path fill-rule="evenodd" d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+                </svg> آپلود عکس شما : </p>
+                <div> <input type="file" name="photo" id="photo"> </div>
+                <br>
+                <p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-in-down" viewBox="0 0 16 16">
+                 <path fill-rule="evenodd" d="M3.5 6a.5.5 0 0 0-.5.5v8a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-8a.5.5 0 0 0-.5-.5h-2a.5.5 0 0 1 0-1h2A1.5 1.5 0 0 1 14 6.5v8a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-8A1.5 1.5 0 0 1 3.5 5h2a.5.5 0 0 1 0 1h-2z"/>
+                    <path fill-rule="evenodd" d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+                     </svg> آپلود مدرک شما : </p>
+                <div> <input type="file" name="certificate"> </div>
+                <br>
+                 </div>
+                <!-- Name -->
+                <div class=" col-6">
+                <div>
+                    <p style="margin: auto; margin-left:10vw"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                </svg>نام</p>
+
+                    <input id="name" class="block mt-1 w-full" type="text" name="name" value="{{$user->name}}" required autofocus />
+                </div>
+
+                <!-- Email Address -->
+                <div class="mt-4">
+                    <p style="margin: auto; margin-left:9vw"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+                    <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
+                    </svg>ایمیل </p>
+                    <input id="email" class="block mt-1 w-full" type="email" name="email" value="{{$user->email}}" required style="margin-left: 5px;" />
+                </div>
+                <!-- phone -->
+                <div class="mt-4">
+                    <p style="margin: auto; margin-left:7vw"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+                    </svg>شماره تلفن</p>
+                 <input id="phone" class="block mt-1 w-full" type="text" name="phone" value="{{$user->phone}}" required style="margin-left: 8px;" />
+                </div>
+                <!-- bio --> 
+            </div>
+            <br>
+		    <br>
+            <hr>
+            <br>
+		    <br>
+            <div class="row divupload" style="margin: auto;">
+            <div class=" col">
+            <div class="mt-4">
+                
+                    <p style="margin: auto;"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-balloon" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8 9.984C10.403 9.506 12 7.48 12 5a4 4 0 0 0-8 0c0 2.48 1.597 4.506 4 4.984ZM13 5c0 2.837-1.789 5.227-4.52 5.901l.244.487a.25.25 0 1 1-.448.224l-.008-.017c.008.11.02.202.037.29.054.27.161.488.419 1.003.288.578.235 1.15.076 1.629-.157.469-.422.867-.588 1.115l-.004.007a.25.25 0 1 1-.416-.278c.168-.252.4-.6.533-1.003.133-.396.163-.824-.049-1.246l-.013-.028c-.24-.48-.38-.758-.448-1.102a3.177 3.177 0 0 1-.052-.45l-.04.08a.25.25 0 1 1-.447-.224l.244-.487C4.789 10.227 3 7.837 3 5a5 5 0 0 1 10 0Zm-6.938-.495a2.003 2.003 0 0 1 1.443-1.443C7.773 2.994 8 2.776 8 2.5c0-.276-.226-.504-.498-.459a3.003 3.003 0 0 0-2.46 2.461c-.046.272.182.498.458.498s.494-.227.562-.495Z"/>
+</svg>بیوگرافی </p>
+                <textarea  name="bio" rows="7" style="font-size:18px;" class="w-75">{{$user->bio}} </textarea>
+                 
+                </div>
+            </div>
+            </div>
+            <div class="flex items-center justify-end mt-4">
+
+                <button id="submitbutton">
+                    {{ __('به روز رسانی') }}
+                </button>
+            </div>
+        </div>
+    </div>
+</form>
+
+@endsection
