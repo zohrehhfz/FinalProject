c95789f38f72d8db825d6b824c2cbd1f7c3ee9eb
zohrehhfz <hfz.zohreh@gmail.com>
2023-02-28 02:13:08 +0330

    show certificate download button

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -37,7 +37,7 @@
 		<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 			<div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
 				<div class="p-6 bg-white border-b border-gray-200" dir=rtl>		
-				@if($user->active != true)
+				@if($user->role == "guide")
 					@if($user->guidepersons->certificatename == NULL)
 					<div class="alert alert-primary d-flex align-items-center" role="alert">
 						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
@@ -64,7 +64,22 @@
 					<img src={{$photo_url}} class="img-fluid img-circle" alt="Profile photo UnAvialable">
 					@endif
 
-					<div class="div1" style="margin: auto; ">
+					<div class="div1" style="margin: auto;">
+					@if($user->role == "guide")
+						@if($user->guidepersons->certificatename != NULL)
+						<button class="certificatebutton"><a href="{{route('ShowCertificate')}}" style="color:white; text-decoration: none;"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;نمایش مدرک <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-award-fill mypen" viewBox="0 0 16 16">
+									<path d="m8 0 1.669.864 1.858.282.842 1.68 1.337 1.32L13.4 6l.306 1.854-1.337 1.32-.842 1.68-1.858.282L8 12l-1.669-.864-1.858-.282-.842-1.68-1.337-1.32L2.6 6l-.306-1.854 1.337-1.32.842-1.68L6.331.864 8 0z" />
+									<path d="M4 11.794V16l4-1 4 1v-4.206l-2.018.306L8 13.126 6.018 12.1 4 11.794z" />
+								</svg></a></button>
+
+						@endif
+						@if($user->active == true)
+						<p>وضعیت: فعال </p>
+						@else
+						<p> وضعیت : غیر فعال</p>
+						@endif
+						@endif
+
 						<p>نام :
 							{{$user->name}}
 						</p>
