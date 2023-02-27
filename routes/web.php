fb34eb575c1d81f5ae7a67edc8f6d05d28eaad99
zohrehhfz <hfz.zohreh@gmail.com>
2023-02-27 21:49:50 +0330

    added route for update, edit profile

--- routes/web.php
+++ routes/web.php
@@ -62,9 +62,11 @@ Route::get('/attractions/show/{attraction}' ,[TouristattractionController::class
 Route::get('/dashboard', [ProfileController::class,'redirectTo'])->middleware(['auth'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/users/changeinfo',[UserController::class, 'edit'])->name('EditUserGuideInfo');
+    Route::post('/users/updateinfo',[UserController::class, 'update'])->name('UpdateUserGuideInfo');
+    //Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    //Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+   // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
