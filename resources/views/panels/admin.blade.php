170ea67085ee1cbc22a99cb23ceb1c27b92d971b
zohrehhfz <hfz.zohreh@gmail.com>
2023-02-28 00:02:59 +0330

    changed margin left

--- resources/views/panels/admin.blade.php
+++ resources/views/panels/admin.blade.php
@@ -29,7 +29,7 @@
 				<path fill-rule="evenodd" d="M5.216 14A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216z" />
 				<path d="M4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z" />
 			</svg>نمایش کاربران</a>
-		<a href="{{route('ShowGuides')}}" class="navbar-brand hover:text-gray-700" style="margin-left:50vw; margin-right:0vw"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people-fill mypen my-2" viewBox="0 0 16 16" style="display:inline;">
+		<a href="{{route('ShowGuides')}}" class="navbar-brand hover:text-gray-700" style="margin-left:47vw; margin-right:0vw"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people-fill mypen my-2" viewBox="0 0 16 16" style="display:inline;">
 				<path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
 				<path fill-rule="evenodd" d="M5.216 14A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216z" />
 				<path d="M4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z" />
