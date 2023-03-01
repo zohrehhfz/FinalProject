a04e67d3dd59ddf5950ddaf65b4801e4dc842841
zohrehhfz <hfz.zohreh@gmail.com>
2023-03-01 12:10:54 +0330

    remove Extra if condition

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,11 +5,9 @@
 	<div class="container-fluid">
 		@if (Route::has('login'))
 		@auth
-		@if(Auth::user()->role == "admin")
-				<a href="{{ route('welcome') }}" class="navbar-brand"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16" style="display:inline;">
+		<a href="{{ route('welcome') }}" class="navbar-brand"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16" style="display:inline;">
   			<path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5Z"/>
 			</svg> خانه </a>
-		@endif
 		<form method="POST" action="{{ route('logout') }}">
 			@csrf
 			<a href="{{ route('logout') }}" onclick="event.preventDefault();
