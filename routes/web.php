4ac424009526b1d3f1e263e8964ea18c90c2500e
zohrehhfz <hfz.zohreh@gmail.com>
2023-03-02 14:31:17 +0330

    added route ShowChat to show nessages to user , guide

--- routes/web.php
+++ routes/web.php
@@ -75,7 +75,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/users/certificate/{user}',[UserController::class, 'certificate'])->name('ShowCertificate');
     Route::post('/users/follow',[UserController::class, 'followUser'])->name('followUser');
     Route::get('/users/followings/{user}',[UserController::class, 'ShowFollowings'])->name('ShowFollowings');
-
+    Route::get('/user/chat/{user}',[ChatController::class, 'ShowChat'])->name('ShowChat');
     //Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     //Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
    // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
