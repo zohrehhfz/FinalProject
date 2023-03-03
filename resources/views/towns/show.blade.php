4b570d450062b280a9c7fe46e936e54dd8c9459f
zohrehhfz <hfz.zohreh@gmail.com>
2023-03-04 01:43:51 +0330

    change view of showattraction's town

--- resources/views/towns/show.blade.php
+++ resources/views/towns/show.blade.php
@@ -61,9 +61,6 @@
 		<br>
 		<br>
 		<p> نام مکان‌های گردشگری واقع در شهر {{$town->name}}</p>
-		@foreach($town->attractions as $attraction)
-		<a style="text-decoration:none"  href="{{route('ShowAttraction',[$attraction])}}"><p> نام مکان گردشگری : {{$attraction->name}}</p></a>
-		@endforeach
 		@if($role == 1)
 				<button id="submitbutton"><a href="{{ route('EditTown',[$town]) }}" style="color:white; text-decoration: none;"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
 							<path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
@@ -72,6 +69,39 @@
 							<path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
 						</svg>  حذف شهر </a></button>
 		@endif
+
+		<div class="container">
+			<div class="" style="margin:auto">
+				<div class="row">
+				@foreach ($town->attractions as $attraction)
+					<div class="col-3">
+					<h5 style="text-align: justify; margin-right:4%;">
+					{{$attraction->name}}
+					</h5>				
+					<p style="text-align: justify; overflow: hidden; text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 3;  line-clamp: 3; -webkit-box-orient: vertical;">
+					{{$attraction->description}}
+					</p>
+				</div>
+					<div class="col-3">
+						<?php
+						$photo_url = Storage::url('public/attractions/' . $attraction->photoname);
+						if ($photo_url == "/storage/attractions/") {
+						?>
+							<img src="/travel-agency.jpg" class="img-fluid img-circle2"style="margin-right:12%; width:80%; height:80%;" alt=" photo Not Set">
+							
+						<?php
+						} else {
+						?>
+								<a href="{{route('ShowAttraction',[$attraction])}}"><img src="{{$photo_url}}" class="img-fluid img-circle2" alt=" photo UnAvialable" style="margin-right:0%; width:100%; height:100%;"></a>
+								<?php
+								}
+								?>
+					</div>
+					@endforeach
+				</div>
+			</div>
+		</div>
+
 	</div>
 	<br>
 	<br>
