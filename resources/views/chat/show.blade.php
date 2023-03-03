e2a3ce89f816051c965c737a99d4db7c6c612cad
zohrehhfz <hfz.zohreh@gmail.com>
2023-03-04 00:51:54 +0330

    added form action for send message

--- resources/views/chat/show.blade.php
+++ resources/views/chat/show.blade.php
@@ -27,7 +27,7 @@
                         <br>
                         <br>
                         <ul>
-                        <div class=" w-100">
+                        <div class=" w-100" style="display:block;">
                                 @foreach($chats as $message)
                                 <?php
                                 $user_id = $message->user_id;
@@ -67,7 +67,9 @@
                                                 <div class="d-inline" style="float: left;"> <?php $v2 = new Verta($message->created_at);
                                                                                             print $v2->formatJalaliDate(); ?></div>
                                             </i>
+                                            
                                         </div>
+                                       
                                         <br> <br>
                                         <div class="pl-2" style=" text-align: right;">{{$message->message}}</div>
                                         <br>
@@ -93,6 +95,7 @@
                                                     {{$message->message}}
                                                 </div>
                                         </div>
+                                        
                                         <div>
                                             
 
@@ -109,13 +112,18 @@
                     <!-- end chat -->
                     <br>
                     <br>
-                    <div class="sub_cm" style="background-color:#A1C4E7; width:auto">
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <div class="sub_cm" style="background-color:#A1C4E7; width:auto; margin-top:2vh; margin-bottom:2vh;">
                         <div class="row">
-                            <form id="commentform" action="" method="post">
+                            <form id="commentform" action="{{route('SendMessage')}}" method="post">
                                 @csrf
+                                <input type="hidden" id="contact" name="contact" value={{ $contact->id }}>
                                 <div style="margin-top:2vh; margin-bottom:2vh;">
-                                    
-                                    
                                     <textarea name="message" id="commentform" cols="70" rows="1" placeholder="پیام خود را بنویسید."></textarea>
                                     <button id="submitbutton" type="submit" style="background-color:#D9E7F3; color:black; width:auto; display:inline; margin:auto;"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-double-left" viewBox="0 0 16 16">
                                             <path fill-rule="evenodd" d="M8.354 1.646a.5.5 0 0 1 0 .708L2.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z" />
